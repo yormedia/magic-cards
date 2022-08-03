393d1f6c4e494bdd03c79198681b34534029a854
yormedia <46250500+yormedia@users.noreply.github.com>
2022-08-03 13:17:36 +0200

    Initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import { NAME, VERSION } from './constants';
 
+export { MagicTableCard } from "./cards/table-card/table-card";
+
 /* eslint no-console: 0 */
 console.info( `%c 🪄 ${NAME} 🪄 \n%c ${VERSION}`, 'color: black; font-weight: bold; background: orange' );
